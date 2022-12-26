707d1a39e1d28d7c92785b8d4cf1a5fea872c598
张欧文 <1534661514@qq.com>
2022-12-26 13:32:02 +0800

    using ajax to send pst request

--- server.js
+++ server.js
@@ -13,6 +13,20 @@ app.get("/server", (request, response) => {
   response.send("Hello Ajax");
 });
 
+app.all("/json-server", (request, response) => {
+  // set response header 设置允许跨域
+  response.setHeader("Access-Control-Allow-Origin", "*");
+  response.setHeader("Access-Control-Allow-Headers", "*");
+
+  const data = {
+    name: "atguigu8848",
+  };
+
+  let str = JSON.stringify(data);
+  // set response body
+  response.send(str);
+});
+
 // listen port
 app.listen(8000, () => {
   console.log("Service has been initialized, 8000 is listening.");
